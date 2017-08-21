86bc2728b95f795e868bbfa02dc5292b0c3485ed
dolonche <ivan.pitenin@gmail.com>
2017-08-22 01:02:51 +0300

    Поправил третье задание

--- js/picture.js
+++ js/picture.js
@@ -1,164 +1,170 @@
-'use strict' ;
-document.querySelector('.upload-form').classList.add('hidden');
+'use strict';
 var gallery = document.querySelector('.gallery-overlay');
-gallery.classList.remove('hidden');
 var picturesList = document.querySelector('.pictures');
 var picturesTemplate = document.querySelector('#picture-template').content;
 var picturesComments = ['Все отлично!', 'В целом все плохо. Но не все'];
-var maxLikes = 201;
-var minLikes = 1;
+document.querySelector('.upload-form').classList.add('hidden');
+gallery.classList.remove('hidden');
 var randomLikes = function () {
-	var likesValue = Math.floor(Math.random() * (maxLikes - minLikes)) + minLikes;
-	return likesValue;
+  var maxLikes = 200;
+  var minLikes = 1;
+  var likesValue = Math.floor(Math.random() * (maxLikes - minLikes)) + minLikes;
+  return likesValue;
 }
 var randomComments = function () {
-	var commentsValue = Math.round(Math.random());
-	return picturesComments[commentsValue];
+  var commentsValue = Math.round(Math.random());
+  return picturesComments[commentsValue];
 }
 var pictures = [
-	{
-		url: 'photos/1.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/2.jpg',
-		likes: randomLikes(),
-		comments: randomComments()
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments()
 	},
-	{
-		url: 'photos/3.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/4.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/5.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/6.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/7.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/8.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/9.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/10.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/11.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/12.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/13.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/14.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/15.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/16.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/17.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/18.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/19.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/20.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/21.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/22.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/23.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/24.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	},
-	{
-		url: 'photos/25.jpg',
-		likes: randomLikes(),
-		comments: randomComments(),
+  {
+    url: 'photos/',
+    likes: randomLikes(),
+    comments: randomComments(),
 	}
 ]
 var renderPictureElement = function (pictures) {
-	var pictureElement = picturesTemplate.cloneNode(true);
-	pictureElement.querySelector('.picture img').src = pictures.url;
-	pictureElement.querySelector('.picture-comments').textContent = pictures.comments;
-	pictureElement.querySelector('.picture-likes').textContent = pictures.likes;
-	return pictureElement;
+  var pictureElement = picturesTemplate.cloneNode(true);
+  var pictureImg = pictureElement.querySelector('.picture img');
+  var pictureComments = pictureElement.querySelector('.picture-comments');
+  var pictureLikes = pictureElement.querySelector('.picture-likes');
+  pictureImg.src = pictures.url + i + '.jpg';
+  pictureComments.textContent = pictures.comments;
+  pictureLikes.textContent = pictures.likes;
+  return pictureElement;
 }
 var renderGalleryItem = function (pictures) {
-	var galleryElement = picturesTemplate.cloneNode(true);
-	gallery.querySelector('.gallery-overlay-image').src = pictures.url;
-	gallery.querySelector('.likes-count').textContent = pictures.likes;
-	gallery.querySelector('.comments-count').textContent = pictures.comments;
-	return galleryElement;
+  var galleryElement = picturesTemplate.cloneNode(true);
+  var galleryImage = gallery.querySelector('.gallery-overlay-image');
+  var likesCount = gallery.querySelector('.likes-count');
+  var commentsCount = gallery.querySelector('.comments-count');
+  galleryImage.src = pictures.url + i + '.jpg';
+  likesCount.textContent = pictures.likes;
+  commentsCount.textContent = pictures.comments;
+  return galleryElement;
 }
 var fragment = document.createDocumentFragment();
-for (var i = 0; i <= pictures.length - 1; i++) {
-	fragment.appendChild(renderPictureElement(pictures[i]));
-	renderGalleryItem(pictures[i]);
+for (var i = 1; i < pictures.length; i++) {
+  fragment.appendChild(renderPictureElement(pictures[i]));
+  renderGalleryItem(pictures[i]);
 }
 picturesList.appendChild(fragment);
