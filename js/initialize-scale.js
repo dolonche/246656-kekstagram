1fbeb2413ae0eafbba5f2b13070bf31f0328f3dd
dolonche <ivan.pitenin@gmail.com>
2017-09-10 17:09:39 +0300

    module5-task3

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,24 +1,24 @@
 'use strict';
 
 window.initializeScale = (function (resizeValue, resizeObject) {
-    var resizeImageValue = function () {
-      resizeObject.style.transform = 'scale' + '(0.' + parseInt(resizeValue.value, 10) + ')';
-    };
-    var resizeControl = document.querySelector('#upload-select-image').querySelector('.upload-resize-controls');
-    resizeControl.addEventListener('click', function (event) {
-      if (event.target.classList[event.target.classList.length - 1] === 'upload-resize-controls-button-dec') {
-        if (parseInt(resizeValue.value, 10) > parseInt(25, 10)) {
-          resizeValue.value = parseInt(resizeValue.value, 10) - parseInt(25, 10) + '%';
-          resizeImageValue(resizeValue);
-        }
-      } else if (event.target.classList[event.target.classList.length - 1] === 'upload-resize-controls-button-inc') {
-        if (parseInt(resizeValue.value, 10) < parseInt(100, 10)) {
-          resizeValue.value = parseInt(resizeValue.value, 10) + parseInt(25, 10) + '%';
-          resizeImageValue(resizeValue);
-          if (parseInt(resizeValue.value, 10) === parseInt(100, 10)) {
-            resizeObject.style.transform = 'scale(1)';
-          }
+  var resizeImageValue = function () {
+    resizeObject.style.transform = 'scale' + '(0.' + parseInt(resizeValue.value, 10) + ')';
+  };
+  var resizeControl = document.querySelector('#upload-select-image').querySelector('.upload-resize-controls');
+  resizeControl.addEventListener('click', function (event) {
+    if (event.target.classList[event.target.classList.length - 1] === 'upload-resize-controls-button-dec') {
+      if (parseInt(resizeValue.value, 10) > parseInt(25, 10)) {
+        resizeValue.value = parseInt(resizeValue.value, 10) - parseInt(25, 10) + '%';
+        resizeImageValue(resizeValue);
+      }
+    } else if (event.target.classList[event.target.classList.length - 1] === 'upload-resize-controls-button-inc') {
+      if (parseInt(resizeValue.value, 10) < parseInt(100, 10)) {
+        resizeValue.value = parseInt(resizeValue.value, 10) + parseInt(25, 10) + '%';
+        resizeImageValue(resizeValue);
+        if (parseInt(resizeValue.value, 10) === parseInt(100, 10)) {
+          resizeObject.style.transform = 'scale(1)';
         }
       }
-    }, true);
+    }
+  }, true);
 });
